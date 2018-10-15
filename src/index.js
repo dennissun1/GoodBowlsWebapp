f5172dce5cf9490a48e53a25fa39860c9abc72e9
qiaofengmarco <qiaofeng.cs05@g2.nctu.edu.tw>
2018-10-14 23:29:39 -0400

    add more css descriptions and rename some files

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom'
 
-ReactDOM.render(
+ReactDOM.hydrate(
     <BrowserRouter>
         <App />
     </BrowserRouter>
