7712f755e317978971c4b89443a94bfad2ddb23d
qiaofengmarco <qiaofeng.cs05@g2.nctu.edu.tw>
2018-10-19 23:53:19 -0400

    merge

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.hydrate(
     <BrowserRouter>
