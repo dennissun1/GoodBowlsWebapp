382568c7117e74c73653689979659c482fd45bbf
Jennifer Li <lilla@live.unc.edu>
2018-10-26 16:36:00 -0400

    rec tabs

--- src/MyMakeOwnBowls.js
+++ src/MyMakeOwnBowls.js
@@ -7,7 +7,6 @@ class MyMakeOwnBowls extends React.Component {
             <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-body {font-family: Arial;}
 
 /* Style the tab */
 .tab {
