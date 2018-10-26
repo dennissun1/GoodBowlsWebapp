fc6dcc013b95ea66102bc95ab58298c3b240d992
qiaofengmarco <qiaofeng.cs05@g2.nctu.edu.tw>
2018-10-26 18:30:56 -0400

    Create three sub-tabs for three bowls

---
+++ src/Bowl1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Bowl1 extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Bowl 1</h1>
+            </div>
+        );
+    }
+}
+
+export default Bowl1;
---
+++ src/Bowl3.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Bowl3 extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Bowl 3</h1>
+            </div>
+        );
+    }
+}
+
+export default Bowl3;
