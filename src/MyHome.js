d474ab3b869ccbc3ce758ef6c9e91297f643ac5e
Jennifer Li <lilla@live.unc.edu>
2018-11-04 21:15:29 -0500

    home page

--- src/MyHome.js
+++ src/MyHome.js
@@ -4,7 +4,17 @@ class My_Feed extends React.Component {
     render() {
         return(
             <div>
-                <h1>Home Page</h1>
+                <h1>Welcome to the Good Bowls Web App!</h1>
+                <img src="Home.jpg" alt="Good Bowls Home">
+                
+                 <a href="https://goodbowlstest.herokuapp.com/map">MAPS</a> 
+                    <p>Learn about where you can purchase Good Bowls and where we source our ingredients.</p>
+            
+                 <a href="https://goodbowlstest.herokuapp.com/feed">FEED</a> 
+                    <p>Stay up to date with the latest information from the Good Bowls team.</p>
+            
+                 <a href="https://goodbowlstest.herokuapp.com/bowl1">RECIPES</a> 
+                    <p>Cook your very own Good Bowls at home with our recipes.</p>
             </div>
         );
     }
