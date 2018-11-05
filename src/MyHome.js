b9778a694b6a7b1d30e995d11a121350a28cf190
Jennifer Li <lilla@live.unc.edu>
2018-11-04 21:28:44 -0500

    home

--- src/MyHome.js
+++ src/MyHome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class My_Feed extends React.Component {
+class My_Home extends React.Component {
     render() {
         return(
             <div>
@@ -8,13 +8,35 @@ class My_Feed extends React.Component {
                 //<img src="Home.jpg" alt="Good Bowls Home">
                 
                  //<a href="https://goodbowlstest.herokuapp.com/map">MAPS</a> 
-                    <p>Learn about where you can purchase Good Bowls and where we source our ingredients.</p>
+                    //<p>Learn about where you can purchase Good Bowls and where we source our ingredients.</p>
             
                  //<a href="https://goodbowlstest.herokuapp.com/feed">FEED</a> 
-                    <p>Stay up to date with the latest information from the Good Bowls team.</p>
+                    //<p>Stay up to date with the latest information from the Good Bowls team.</p>
             
                  //<a href="https://goodbowlstest.herokuapp.com/bowl1">RECIPES</a> 
-                    <p>Cook your very own Good Bowls at home with our recipes.</p>
+                    //<p>Cook your very own Good Bowls at home with our recipes.</p>
+            
+                <h3>Ingredients</h3>
+                <p> 1 head cauliflower<br />
+                    1 chicken breast, diced<br />
+                    1 cup  peas<br />
+                    1 cup  carrot<br />
+                    2 tablespoons  soy sauce<br />
+                    2 teaspoons  black pepper<br />
+                    3 eggs, beaten<br />
+                    2 tablespoons  sesame seed
+                </p>
+
+                <h3>Preparation</h3>
+                <ol>
+                  <li>Remove all leaves and cut the cauliflower into small florets. Place the florets in a food processor and blend until they are a rice-like texture. Be careful not to over process or it may turn mushy. Set aside, and drain excess moisture with a towel, if desired.</li>
+                  <li>In a large saucepan over medium-high heat, cook the chicken until no pink is showing.</li>
+                  <li>Add the peas, carrots, soy sauce, and black pepper.</li>
+                  <li>Stir until most of the liquid has reduced, then add the cauliflower rice. Cook for about 3-4 minutes until the vegetables and chicken are incorporated evenly into the cauliflower.</li>
+                  <li>Spread the rice towards the edge of the pan, creating a well in the center. Pour the beaten eggs into the well, stirring only the eggs until they are cooked.</li>
+                  <li>Fold the eggs into the rice, then mix in the sesame seeds.</li>
+                </ol>
+
             </div>
         );
     }
