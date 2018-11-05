84374565b7018cc6d96a6f7ab729cd0c64069900
qiaofengmarco <qiaofeng.cs05@g2.nctu.edu.tw>
2018-11-05 01:09:12 -0500

    Home

--- src/MyHome.js
+++ src/MyHome.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 import HomeImg from "./Home.jpg"
 
-<<<<<<< HEAD
 class MyHome extends React.Component {
     render() {
         return(
@@ -13,32 +12,8 @@ class MyHome extends React.Component {
                 <p>Learn about where you can purchase Good Bowls and where we source our ingredients. Visit <NavLink to='/map'>Map</NavLink>.</p>
                 <p>Stay up to date with the latest information from the Good Bowls team. Visit <NavLink to='/feed'>Feed</NavLink>.</p>
                 <p>Cook your very own Good Bowls at home with our recipes. Visit <NavLink to='/recipes'>Recipes</NavLink>.</p>
-=======
-class My_Home extends React.Component {
-    render() {
-        return(
-            <div className = "Home">
-                
-            <h1>Welcome to the Good Bowls WebApp!</h1>
-
-<a href="https://goodbowlstest.herokuapp.com/map/">MAP</a> 
-<p>Learn about where you can purchase Good Bowls and where we source our ingredients.</p>
-
-<a href="https://goodbowlstest.herokuapp.com/feed/">FEED</a> 
-<p>Stay up to date with the latest information from the Good Bowls team.</p>
-
-<a href="https://goodbowlstest.herokuapp.com/recipes/">RECIPES</a> 
-<p>Cook your very own Good Bowls at home with our recipes.</p>
-
-            
->>>>>>> 075af49d9f6a7f2b7fb08f27b51cfd0e5f13bd15
             </div>
         );
     }
 }
-
-<<<<<<< HEAD
 export default MyHome;
-=======
-export default My_Home;
->>>>>>> 075af49d9f6a7f2b7fb08f27b51cfd0e5f13bd15
