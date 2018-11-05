d4674318d529d676a02fe3b1009bbd059055ed8e
Dennis <dsun96@gmail.com>
2018-11-05 03:23:32 -0500

    test db code in server.js

--- server/index.js
+++ server/index.js
@@ -5,23 +5,3 @@ require('babel-register')({
 });
 
 require('./server');
-
-console.log("test");
-
-//db code
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT * FROM map;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
