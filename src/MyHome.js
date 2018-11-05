700e03731b6d2d7232a92526d33e412470e0c7ba
Jennifer Li <lilla@live.unc.edu>
2018-11-04 21:45:38 -0500

    home

--- src/MyHome.js
+++ src/MyHome.js
@@ -4,18 +4,19 @@ class My_Home extends React.Component {
     render() {
         return(
             <div className = "Home">
-                <h1>Welcome to the Good Bowls WebApp!</h1>
                 
-                //<img src="Home.jpg" alt="Good Bowls Home">
+            <h1>Welcome to the Good Bowls WebApp!</h1>
+
+<a href="https://goodbowlstest.herokuapp.com/map/">MAP</a> 
+<p>Learn about where you can purchase Good Bowls and where we source our ingredients.</p>
+
+<a href="https://goodbowlstest.herokuapp.com/feed/">FEED</a> 
+<p>Stay up to date with the latest information from the Good Bowls team.</p>
+
+<a href="https://goodbowlstest.herokuapp.com/recipes/">RECIPES</a> 
+<p>Cook your very own Good Bowls at home with our recipes.</p>
+
             
-                //<a href="https://goodbowlstest.herokuapp.com/map">MAPS</a> 
-                <p>Learn about where you can purchase Good Bowls and where we source our ingredients.</p>
-            
-                 //<a href="https://goodbowlstest.herokuapp.com/feed">FEED</a> 
-                <p>Stay up to date with the latest information from the Good Bowls team.</p>
-            
-                 //<a href="https://goodbowlstest.herokuapp.com/bowl1">RECIPES</a> 
-                 <p>Cook your very own Good Bowls at home with our recipes.</p>
             </div>
         );
     }
