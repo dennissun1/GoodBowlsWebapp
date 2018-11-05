3e6608ca7cc2af5b72042c7c14f137adb9e2a291
qiaofengmarco <qiaofeng.cs05@g2.nctu.edu.tw>
2018-11-05 01:01:40 -0500

    For Homepage

--- src/MyHome.js
+++ src/MyHome.js
@@ -1,23 +1,20 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+import HomeImg from "./Home.jpg"
 
-class My_Feed extends React.Component {
+class MyHome extends React.Component {
     render() {
         return(
             <div>
                 <h1>Welcome to the Good Bowls Web App!</h1>
-                <img src="Home.jpg" alt="Good Bowls Home">
-                
-                 <a href="https://goodbowlstest.herokuapp.com/map">MAPS</a> 
-                    <p>Learn about where you can purchase Good Bowls and where we source our ingredients.</p>
-            
-                 <a href="https://goodbowlstest.herokuapp.com/feed">FEED</a> 
-                    <p>Stay up to date with the latest information from the Good Bowls team.</p>
-            
-                 <a href="https://goodbowlstest.herokuapp.com/bowl1">RECIPES</a> 
-                    <p>Cook your very own Good Bowls at home with our recipes.</p>
+                <img src = {HomeImg} alt = "home"/>
+                <p/>
+                <p>Learn about where you can purchase Good Bowls and where we source our ingredients. Visit <NavLink to='/map'>Map</NavLink>.</p>
+                <p>Stay up to date with the latest information from the Good Bowls team. Visit <NavLink to='/feed'>Feed</NavLink>.</p>
+                <p>Cook your very own Good Bowls at home with our recipes. Visit <NavLink to='/recipes'>Recipes</NavLink>.</p>
             </div>
         );
     }
 }
 
-export default My_Feed;
+export default MyHome;
