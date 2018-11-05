85b424757d29e54b1ccb9a224e52378ad206f164
Jennifer Li <lilla@live.unc.edu>
2018-11-04 21:39:37 -0500

    home

--- src/MyHome.js
+++ src/MyHome.js
@@ -6,15 +6,15 @@ class My_Home extends React.Component {
             <div className = "Home">
                 <h1>Welcome to the Good Bowls WebApp!</h1>
                 
-                <img src="Home.jpg" alt="Good Bowls Home">
+                //<img src="Home.jpg" alt="Good Bowls Home">
             
-                <a href="https://goodbowlstest.herokuapp.com/map">MAPS</a> 
+                //<a href="https://goodbowlstest.herokuapp.com/map">MAPS</a> 
                 <p>Learn about where you can purchase Good Bowls and where we source our ingredients.</p>
             
-                 <a href="https://goodbowlstest.herokuapp.com/feed">FEED</a> 
+                 //<a href="https://goodbowlstest.herokuapp.com/feed">FEED</a> 
                 <p>Stay up to date with the latest information from the Good Bowls team.</p>
             
-                 <a href="https://goodbowlstest.herokuapp.com/bowl1">RECIPES</a> 
+                 //<a href="https://goodbowlstest.herokuapp.com/bowl1">RECIPES</a> 
                  <p>Cook your very own Good Bowls at home with our recipes.</p>
             </div>
         );
