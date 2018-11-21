082a01ba4048219bbe785ca01fc8631242229844
qiaofengmarco <qiaofeng.cs05@g2.nctu.edu.tw>
2018-11-21 17:06:43 -0500

    Fix the look of the navigation button

---
+++ build/precache-manifest.28d9cd78fa65fe1327e72e118f0dfc2d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "45f09e15fe513d7269f9",
+    "url": "/static/css/main.1e39fcf6.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e11b0a1448b6462172dc",
+    "url": "/static/js/1.e11b0a14.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c5aeb2966849452968a24450049a83d0",
+    "url": "/static/media/Home.c5aeb296.jpg"
+  },
+  {
+    "revision": "45f09e15fe513d7269f9",
+    "url": "/static/js/main.45f09e15.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "e11b0a1448b6462172dc",
+    "url": "/static/css/1.f04a1312.chunk.css"
+  },
+  {
+    "revision": "6794c3495f21539eb6aae9a940c85e1b",
+    "url": "/index.html"
+  }
+];
