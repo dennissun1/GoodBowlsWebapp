8fe12db06cb97c145917bd5982e5daca749a97f6
Dennis <dsun96@gmail.com>
2018-11-26 14:37:24 -0500

    merge build folder with css changes

---
+++ build/precache-manifest.111eb82cbdc41ef1b6b61724ad9d3f4c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "10eb580c7a9a2dff2dd59669dbcf1d92",
+    "url": "/static/media/cooking.10eb580c.png"
+  },
+  {
+    "revision": "e0f10acc4922787c1cfa",
+    "url": "/static/css/main.e06d68bf.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7b6cffaf8f92355747a1",
+    "url": "/static/js/1.7b6cffaf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80f50388100311d8d02776464a95332d",
+    "url": "/static/media/Map.80f50388.png"
+  },
+  {
+    "revision": "e0f10acc4922787c1cfa",
+    "url": "/static/js/main.e0f10acc.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "7b6cffaf8f92355747a1",
+    "url": "/static/css/1.f9e17eb7.chunk.css"
+  },
+  {
+    "revision": "60331fa1834242b5dce86a6534bb6df6",
+    "url": "/index.html"
+  }
+];
