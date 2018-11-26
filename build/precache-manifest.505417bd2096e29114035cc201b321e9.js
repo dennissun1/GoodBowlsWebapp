912148112a614b3e9ad9a9038e5234e843e7a78a
Dennis <dsun96@gmail.com>
2018-11-26 12:45:06 -0500

    building for test

---
+++ build/precache-manifest.505417bd2096e29114035cc201b321e9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "0d85d1f11e75ad49c2e0",
+    "url": "/static/css/main.b79b06cd.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "95411a44a2829a2c1e6d",
+    "url": "/static/js/1.95411a44.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c5aeb2966849452968a24450049a83d0",
+    "url": "/static/media/Home.c5aeb296.jpg"
+  },
+  {
+    "revision": "0d85d1f11e75ad49c2e0",
+    "url": "/static/js/main.0d85d1f1.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "95411a44a2829a2c1e6d",
+    "url": "/static/css/1.5e3bc1f2.chunk.css"
+  },
+  {
+    "revision": "a95ab6c358d284f499c9d06a0b38cc91",
+    "url": "/index.html"
+  }
+];
