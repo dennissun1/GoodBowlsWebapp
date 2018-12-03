e3159036dc406022bd1364ca8aa39dd8357c068a
Jennifer Li <lilla@live.unc.edu>
2018-12-02 21:22:46 -0500

    diane edits

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69962113e2c7611f8763adc2013b81dc.js"
+  "/precache-manifest.dd27fec501bd103ad073419d89bca86a.js"
 );
 
 workbox.clientsClaim();
