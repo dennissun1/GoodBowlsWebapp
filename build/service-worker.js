8be8ba5d5db12f66eacb32a9110d080187024c1d
qiaofengmarco <qiaofeng.cs05@g2.nctu.edu.tw>
2018-12-07 01:02:00 -0500

    Third try

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3dc11a03843086659f71523ef89261c5.js"
+  "/precache-manifest.939dd0b4c334ffb1c399f732ccbe6550.js"
 );
 
 workbox.clientsClaim();
--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import MyMain from './MyMain';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        document.addEventListener('touchmove', function (event) {
+            if (event.scale !== 1) { event.preventDefault(); }
+        }, false);
+    }
     render() {
         return (
             <div>
