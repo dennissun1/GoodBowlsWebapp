3b80cdb270c2e56c3a17eac6575854f99cdf53f9
qiaofengmarco <qiaofeng.cs05@g2.nctu.edu.tw>
2018-12-09 15:23:41 -0500

    Project Build for Damjan

---
+++ build/precache-manifest.e513afbdad453d65dbfe1918c8761273.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "10eb580c7a9a2dff2dd59669dbcf1d92",
+    "url": "/static/media/cooking.10eb580c.png"
+  },
+  {
+    "revision": "d76c7c87f4d625c09eab",
+    "url": "/static/css/main.0fb13b48.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "901083a4f3699a0d42ad",
+    "url": "/static/js/1.901083a4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80f50388100311d8d02776464a95332d",
+    "url": "/static/media/Map.80f50388.png"
+  },
+  {
+    "revision": "d76c7c87f4d625c09eab",
+    "url": "/static/js/main.d76c7c87.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "901083a4f3699a0d42ad",
+    "url": "/static/css/1.5c5b23ca.chunk.css"
+  },
+  {
+    "revision": "e616725c62b6d4202274047f0d8873b5",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import {Router} from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
